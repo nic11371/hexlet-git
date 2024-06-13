54c772d3ddc42d5a6b27b5e3c06d877305c326b4
Nikolay Melnikov <nic11371@gmail.com>
2024-06-13 17:06:49 +0300

    happy_numbers_finished

--- happ.py
+++ happ.py
@@ -1,15 +1,23 @@
 def sum_of_square_digits(num):
     integer_square = 0
     for i in str(num):
-        count = int(i) ** 2
-        integer_square += count
+        prev = int(i) ** 2
+        integer_square += prev
     return integer_square
 
-# def is_happy_number(num):
-#   pass
+
+def is_happy_number(num):
+    value_number = sum_of_square_digits(num)
+    count = 1
+    while count <= 10:
+        value_number = sum_of_square_digits(str(value_number))
+        count += 1
+    if value_number == 1:
+        return True
+    return False
 
 
 
 
-print(sum_of_square_digits(23))
-# print(is_happy_number(7))
+# print(sum_of_square_digits(23))
+print(is_happy_number(13))

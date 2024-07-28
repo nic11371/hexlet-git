c823b96ce0b196bc45788f25738902af86792b59
nic11371 <nic11371@gmail.com>
2024-07-28 22:24:18 +0300

    pair_closure

--- pair.py
+++ pair.py
@@ -13,3 +13,21 @@ def car(pair):
 
 pair = cons(4, 5)
 print(car(pair))
+
+
+def cons_1(head, tail):
+    return lambda selector: selector(head, tail)
+
+
+# BEGIN (write your solution here)
+def car_1(pair):
+    return pair(lambda head, tail: head)
+
+
+def cdr_1(pair):
+    return pair(lambda head, tail: tail)
+
+
+pair_1 = cons(5, 3)
+print(car_1(pair_1))
+print(cdr_1(pair_1))

8f4c667e6999b63366dcbc75c6b365982b0c73fb
nic11371 <nic11371@gmail.com>
2024-08-28 23:00:55 +0300

    merges

--- INPUT_OUT/merge.py
+++ INPUT_OUT/merge.py
@@ -1,18 +1,31 @@
+def make_diff_view(diff1, diff2, fout):
+    fout.writelines(f"{'>>>file1>>>'}\n")
+    fout.writelines(f"{''.join(diff1)}=====\n")
+    fout.writelines(f"{''.join(diff2)}")
+    fout.writelines(f"{'<<<file2<<<'}\n")
+
+
 def merge(file1, file2, out):
-    with open(file1, 'r') as input_file1:
-        with open(file2, 'r') as input_file2:
-            with open(out, 'w') as output:
-                for s1, s2 in zip(input_file1, input_file2):
-                    s1 = s1.split()
-                    s2 = s2.split()
-                    if s1 == s2:
-                        output.writelines(f"{' '.join(s1 and s2)}\n")
-                    else:
-                        output.writelines(f"{'>>>file1>>>'}\n")
-                        output.writelines(f"{' '.join(s1)}\n")
-                        output.writelines(f"{'===='}\n")
-                        output.writelines(f"{' '.join(s2)}\n")
-                        output.writelines(f"{'<<<file2<<<'}\n")
+    diff1 = []
+    diff2 = []
+    with open(file1) as input1, open(file2) as input2, open(out, 'w') as fout:
+        for s1, s2 in zip(input1, input2):
+            if s1 == s2:
+                if diff1 or diff2:
+                    make_diff_view(diff1, diff2, fout)
+                    diff1 = []
+                    diff2 = []
+                fout.writelines(f"{''.join(s1)}")
+            diff1.append(s1)
+            diff2.append(s2)
+            # fout.writelines(make_diff_view(diff1, diff2))
+    # return diff1, diff2
+            # else:
+            #     fout.writelines(f"{'>>>file1>>>'}\n")
+            #     fout.writelines(f"{' '.join(s1)}\n")
+            #     fout.writelines(f"{'===='}\n")
+            #     fout.writelines(f"{' '.join(s2)}\n")
+            #     fout.writelines(f"{'<<<file2<<<'}\n")
 
 
 print(merge('./INPUT_OUT/text1.txt',

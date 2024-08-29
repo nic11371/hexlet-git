812accfb198507ee6143a3010bbb41f32d9ea328
Nikolay Melnikov <nic11371@gmail.com>
2024-08-29 11:04:48 +0300

    mergeng

--- INPUT_OUT/merge.py
+++ INPUT_OUT/merge.py
@@ -1,18 +1,35 @@
+# def make_diff_view(diff1, diff2):
+#     line1 = []
+#     line2 = []
+#     line1.append(diff1)
+#     line2.append(diff2)
+#     return line1, line2
+
+
 def merge(file1, file2, out):
-    with open(file1, 'r') as input_file1:
-        with open(file2, 'r') as input_file2:
-            with open(out, 'w') as output:
-                for s1, s2 in zip(input_file1, input_file2):
-                    s1 = s1.split()
-                    s2 = s2.split()
-                    if s1 == s2:
-                        output.writelines(f"{' '.join(s1 and s2)}\n")
-                    else:
-                        output.writelines(f"{'>>>file1>>>'}\n")
-                        output.writelines(f"{' '.join(s1)}\n")
-                        output.writelines(f"{'===='}\n")
-                        output.writelines(f"{' '.join(s2)}\n")
-                        output.writelines(f"{'<<<file2<<<'}\n")
+    str1 = []
+    str2 = []
+    with open(file1) as f1, open(file2) as f2, open(out, 'w') as fout:
+        for diff1, diff2 in zip(f1, f2):
+            if diff1 != diff2:
+                str1.append(diff1)
+                str2.append(diff2)
+            fout.write(f"{"".join(str1)}")
+            # if diff1 or diff2:
+            #     fout.write(make_diff_view(diff1, diff2))
+            
+
+                    # if s1 == s2:
+                    #     output.writelines(f"{' '.join(s1 and s2)}\n")
+                    # else:
+                    #     output.writelines(f"{'>>>file1>>>'}\n")
+                    #     output.writelines(f"{' '.join(s1)}\n")
+                    #     output.writelines(f"{'===='}\n")
+                    #     output.writelines(f"{' '.join(s2)}\n")
+                    #     output.writelines(f"{'<<<file2<<<'}\n")
+
+#  if diff1 or diff2: 
+    #  fout.write(make_diff_view(diff1, diff2)) diff1 = [] diff2 = []
 
 
 print(merge('./INPUT_OUT/text1.txt',

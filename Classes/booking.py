5357bf0429167e4a33841883d2e40cc7f9f5dda9
Nic11371 <nic11371@gmail.com>
2024-09-23 19:50:58 +0300

    booking

--- Classes/booking.py
+++ Classes/booking.py
@@ -8,7 +8,7 @@ class Booking:
     def book(self, start, stop):
         start_b = date.fromisoformat(start)
         stop_b = date.fromisoformat(stop)
-        
+
         range_book = (start_b, stop_b)
 
         if start_b == stop_b or start_b > stop_b:
@@ -17,38 +17,39 @@ class Booking:
         if not self.mem:
             self.mem.append(range_book)
             return True
-        
-        for elem in self.mem:
-            if elem[0] < start_b < elem[1]:
+
+        for dates in self.mem:
+            date_start, date_stop = dates
+            if date_start < start_b < date_stop:
                 return False
-            elif elem[0] < stop_b < elem[1]:
+            if date_start < stop_b < date_stop:
                 return False
-            else:
-                return True
+        self.mem.append(range_book)
+        return True
 
 
-booking = Booking()
-print(booking.book('2008-11-11', '2008-11-13'))  # True
-print(booking.book('2008-11-12', '2008-11-12'))  # False
-print(booking.book('2008-11-10', '2008-11-12'))  # False
-print(booking.book('2008-11-12', '2008-11-14'))  # False
-print(booking.book('2008-11-10', '2008-11-11'))  # True
-print(booking.book('2008-11-13', '2008-11-14'))  # True
+# booking = Booking()
+# print(booking.book('2008-11-11', '2008-11-13'))  # True
+# print(booking.book('2008-11-12', '2008-11-12'))  # False
+# print(booking.book('2008-11-10', '2008-11-12'))  # False
+# print(booking.book('2008-11-12', '2008-11-14'))  # False
+# print(booking.book('2008-11-10', '2008-11-11'))  # True
+# print(booking.book('2008-11-13', '2008-11-14'))  # True
 
 
-# def test_booking():
-#     booking = Booking()
+def test_booking():
+    booking = Booking()
 
-#     assert not booking.book('2008-11-10', '2008-11-05')
-#     assert booking.book('2008-11-11', '2008-11-13')
-#     assert not booking.book('2008-11-12', '2008-11-12')
-#     assert not booking.book('2008-11-12', '2008-11-14')
-#     assert booking.book('2008-11-10', '2008-11-11')
-#     assert not booking.book('2008-11-12', '2008-11-13')
-#     assert not booking.book('2008-11-13', '2008-11-13')
-#     assert booking.book('2008-11-13', '2008-11-14')
-#     assert booking.book('2008-05-08', '2008-05-18')
-#     assert not booking.book('2008-05-09', '2008-05-10')
+    assert not booking.book('2008-11-10', '2008-11-05')
+    assert booking.book('2008-11-11', '2008-11-13')
+    assert not booking.book('2008-11-12', '2008-11-12')
+    assert not booking.book('2008-11-12', '2008-11-14')
+    assert booking.book('2008-11-10', '2008-11-11')
+    assert not booking.book('2008-11-12', '2008-11-13')
+    assert not booking.book('2008-11-13', '2008-11-13')
+    assert booking.book('2008-11-13', '2008-11-14')
+    assert booking.book('2008-05-08', '2008-05-18')
+    assert not booking.book('2008-05-09', '2008-05-10')
 
 
-# test_booking()
+test_booking()

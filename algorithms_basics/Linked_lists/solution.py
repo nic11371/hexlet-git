5f6646b4ad24ca39923858dabcc84f894a268ff2
Nic11371 <nic11371@gmail.com>
2024-10-22 22:05:55 +0300

    link

--- algorithms_basics/Linked_lists/solution.py
+++ algorithms_basics/Linked_lists/solution.py
@@ -6,16 +6,17 @@ from algorithms_basics.Linked_lists.helpers import get_linked_list_from_array
 sys.path.append(os.path.abspath('/usr/src/app/'))
 
 
-def solution(lists):
-    arr = []
-    link = get_linked_list_from_array(lists)
-    arr.append(link)
-    linked_list = LinkedList()
-    # linked_list = LinkedList()
-    return arr
+def solution(arr):
+    linked_list = get_linked_list_from_array(arr)
+    head = linked_list.head
+    tail = linked_list.tail
+    while head:
+        linked_list.prepend(head.next)
+    reverse = LinkedList.to_array(linked_list)
+    return reverse
 
 
 items = [[10, 20], 0, -1, ['hey']]
 
 print(solution(items))  # [['hey'], -1, 0, [10, 20]]
-solution([])  # []
+# solution([])  # []

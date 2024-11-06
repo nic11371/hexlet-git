39a345fb32bfc2aa563b9c19f08da911d7cffbba
Nikolay Melnikov <nic11371@gmail.com>
2024-11-06 09:50:00 +0300

    hash_tables

--- algorithms_basics/hashes/solution.py
+++ algorithms_basics/hashes/solution.py
@@ -12,42 +12,44 @@ def remove(hash, key):
     if record is None:
         return None
 
-    while record and record.value['key'] == key:
+    if record and record.value['key'] == key:
         removed = record.value['value']
         record = record.next
-        hash.count = - 1
+        hash.count -= 1
     # current_node = record
 
     while record.next is not None:
         if record.next.value['key'] == key:
             removed = record.next.value['value']
             record.next = record.next.next
-            hash.count =- 1
+            hash.count -= 1
         else:
             record = record.next
 
-    if hash.table[index].tail.value['key'] is not None:
-        hash.table[index].tail = record
+    # if hash.table[index].tail.value['key'] is not None:
+    #     hash.table[index].tail = record
 
     return removed
 
 
-# def solution(map, key):
+# def solution(key):
 #     hash = Hash()
-#
-#     for key_, value in map.items():
-#         hash.set(key_, value)
-#
-#     return remove(hash, key)
+
+    # for key_, value in map.items():
+    #     hash.set(key_, value)
+
+    # return remove(hash, key)
 
 
 table = Hash()
 table.set("key", "value")
 table.set("key1", "value1")
-print(remove(table, 'key'))
 
+removed = remove(table, 'key')
+print(removed)
 # removed = solution(table, "key")
 # print(removed)  # => value
+# print(table)
 
 # В хеше ключа больше нет
 print(table.get("key"))  # None

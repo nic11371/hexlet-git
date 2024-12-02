8de9c1b9898e4d9ea59ecc0e8be533074722edcb
Nikolay Melnikov <nic11371@gmail.com>
2024-12-02 09:18:49 +0300

    BTree_2

--- Algoritms_trees/BTree/solution.py
+++ Algoritms_trees/BTree/solution.py
@@ -26,3 +26,25 @@ def solution(items, min, max):
 print(solution(data, 30, 50))
 
 # END
+
+# def find_values_in_range(node, min, max):
+#     values = []
+#     keys = node.keys
+#     children = node.children
+
+#     # Поиск значений в текущем узле
+#     for i in range(len(keys)):
+#         if keys[i] >= min and keys[i] <= max:
+#             values.append(keys[i])
+
+#     # Рекурсивный поиск значений в дочерних узлах
+#     if not node.leaf:
+#         for i in range(len(children)):
+#             if i == 0 and keys[0] >= min:
+#                 values.extend(find_values_in_range(children[i], min, max))
+#             elif i == len(children) - 1 and keys[-1] <= max:
+#                 values.extend(find_values_in_range(children[i], min, max))
+#             elif i < len(children) - 1 and (keys[i] >= min and keys[i] <= max):
+#                 values.extend(find_values_in_range(children[i], min, max))
+
+#     return values

9503d5ef10ce7cf320a3563729107e20c6ff342c
Nikolay Melnikov <nic11371@gmail.com>
2025-05-26 17:10:12 +0300

    parsing photo

--- telethone/1-login.py
+++ telethone/1-login.py
@@ -1,4 +1,6 @@
 from telethon import TelegramClient, events, sync, connection
+import asyncio
+import os
 
 
 api_id = 26102772
@@ -8,3 +10,15 @@ client = TelegramClient('session_name', api_id, api_hash, system_version="4.10.5
 client.start()
 print(client.get_me())
 # client.disconnect()
+
+participants = client.get_participants('https://t.me/Parsinger_Telethon_Test')
+for i, user in enumerate(participants):
+    client.download_profile_photo(user, f'{i}', download_big=False)
+    print(user.id, user.first_name, user.last_name, user.phone)
+
+def get_size(folder: str):
+    return sum(
+        [os.path.getsize(f"{folder}/{file}") for file in os.listdir(folder)]
+    )
+
+get_size('C:/Users/melnikov.nn/Documents/phyton/Lessons/2-hexlet-git/telethone')

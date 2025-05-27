7fbc35455c3c32b15d9f16a4dd105def72518c86
Nikolay Melnikov <nic11371@gmail.com>
2025-05-27 16:47:42 +0300

    exceptions

---
+++ asyncio/exceptions/1-exception.py
@@ -0,0 +1,31 @@
+import asyncio
+
+# files = ['image.png', 'file.csv', 'file1.txt'... ]  полный список вшит в задачу
+# missed_files = [...] список пропущенных файлов "спрятан" внутри задачи
+
+# Не менять функцию
+async def download_file(file_name):
+    await asyncio.sleep(1)
+    if file_name in missed_files:
+        raise FileNotFoundError(f'Файл {file_name} не найден')
+    else:
+        await asyncio.sleep(1)
+        return f'Файл {file_name} успешно скачан'
+
+# Ваш код пишите тут:    
+async def main():
+    tasks = []
+    for file in files:
+        task = asyncio.create_task(download_file(file))
+        tasks.append(task)
+    try:
+        await asyncio.gather(*tasks, return_exceptions=True)
+    except FileNotFoundError as e:
+        print(e)
+    for task in tasks:
+        exception = task.exception()
+        if exception:
+            print(exception)
+
+
+asyncio.run(main())
--- telethone/8-sent_message.py
+++ telethone/8-sent_message.py
@@ -8,4 +8,4 @@ api_hash = '54a0b7d2b93fb51c50807d85e18ecd40'
 result = []
 with TelegramClient('my', api_id, api_hash, system_version="4.10.5 beta x64") as client:
     client.send_message('@cwadventure', 'test')
-cd 
+    

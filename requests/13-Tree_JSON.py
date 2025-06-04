0ae10f5f201f956bdc1203fd30e13979cb45d9b3
Nikolay Melnikov <nic11371@gmail.com>
2025-06-04 11:00:40 +0300

    tree JSON

--- requests/13-Tree_JSON.py
+++ requests/13-Tree_JSON.py
@@ -1,17 +1,36 @@
 import requests
-from collections import Counter
+
 
 url = 'https://parsinger.ru/3.4/3/dialog.json'
 
 response = requests.get(url, verify=False)
 
-members = [response.json()]
+members_resp = [response.json()]
 
 dictionary = {}
 
-def count_members(member):
+
+def count_members(members):
+    count = 1
     for member in members:
         username = member['username']
-        print(username)
-#         dictionary[username] = Counter(username.values())
-# print(dictionary)
+        if username in dictionary:
+            dictionary[username] += 1
+        else:
+            dictionary[username] = count
+        comments = member['comments']
+        if not comments:
+            continue
+        else:
+            count_members(comments)
+    return dictionary
+
+
+def dictionary_sort():
+    dictionary_count = count_members(members_resp)
+    dictionary_sorted = dict(
+        sorted(dictionary_count.items(), key=lambda x: (-x[-1], x[0])))
+    return dictionary_sorted
+
+
+print(dictionary_sort())
